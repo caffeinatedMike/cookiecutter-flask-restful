6a446c586327770fdf41d50b98bbb52c0ac1ea6c
Mike Hill <mhilluniversal@gmail.com>
2021-11-05 10:33:13 -0400

    Updated cli command name

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/manage.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/manage.py
@@ -2,15 +2,20 @@ import click
 from flask.cli import with_appcontext
 
 
-@click.command("init")
+@click.command("create-admin")
 @with_appcontext
-def init():
+def create_admin():
     """Create a new admin user"""
     from {{cookiecutter.app_name}}.extensions import db
     from {{cookiecutter.app_name}}.models import User
 
-    click.echo("create user")
-    user = User(username="{{cookiecutter.admin_user_username}}", email="{{cookiecutter.admin_user_email}}", password="{{cookiecutter.admin_user_password}}", active=True)
+    click.echo("creating user 'admin'")
+    user = User(
+        username="{{cookiecutter.admin_user_username}}",
+        email="{{cookiecutter.admin_user_email}}",
+        password="{{cookiecutter.admin_user_password}}",
+        active=True
+    )
     db.session.add(user)
     db.session.commit()
-    click.echo("created user admin")
+    click.echo("created user 'admin'")
